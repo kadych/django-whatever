54b0a22ebb6ee3e8317eb41cf71b6f67e2aa3ce1
Sergey Fursov <GeyseR85@gmail.com>
2014-01-28 10:13:45 +0300

    pass all errors when can't import
    For example we cant import django specific models (e.g. ContentType) during installation when obtain current version of app

--- django_any/__init__.py
+++ django_any/__init__.py
@@ -3,6 +3,6 @@ __version__ = (0, 2, 3, 'final', 0)
 try:
     from django_any.forms import any_form_field, any_form
     from django_any.models import any_field, any_model
-except ImportError:
+except:
     pass
 
